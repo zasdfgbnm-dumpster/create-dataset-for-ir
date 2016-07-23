ae135a4d8ea526c83fcb1f2a3568e1a20a2b232e
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-07-23 23:32:18 +0800

    completely work

--- 04_create_expir_table.scala
+++ 04_create_expir_table.scala
@@ -1,134 +1,158 @@
 import scala.math._
-import sparkSession.implicits._
 import org.apache.spark.sql._
 import org.apache.spark._
-import scala.collection.mutable._
+import scala.collection.mutable
+import scala.collection.mutable.PriorityQueue
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+import sys.process._
+import scala.language.postfixOps
 
-def wl2wn(wl:Float):Float = 10000 / wl
+object CreateExpIRTable {
 
-def absorb2trans(absorb:Float,yfactor:Float):Float = pow(10, yfactor*absorb)
+    class BadJDXException(info:String) extends Throwable {}
 
-def parse_state(state:String):(String,String) = {
-    val statesplit = state.trim().split(raw"\s+",2).map(_.trim())
-    (statesplit[0],statesplit[1])
+    val inputdir = "/home/gaoxiang/create-dataset-for-ir/outputs/01/digitalized/"
+
+    def parse_state(state:String):(String,String) = {
+        val pattern = raw"(\w*)(.*)".r
+        val pattern(s,i) = state
+        val ss = s.toLowerCase.trim
+        if(Array("gas","solid","liquid","solution").contains(ss))
+            (ss,i.trim)
+        else
+            ("others",state)
+    }
+
+    // convert x,y data to points at standard x
+    // Note: xyxy must be ordered ascendingly
+    // How to let PriorityQueue order ascending?
+    //   val pq = PriorityQueue.empty[(Int, String)](implicitly[Ordering[(Int, String)]].reverse)
+    def standard(xyxy:PriorityQueue[(Float,Float)]):Array[Float] = {
+        val xmin = 670 //included
+        val xmax = 3702 //included
+        val xstep = 4
+        val vecsize = (xmax-xmin)/xstep + 1
+        val yy = new Array[Float](vecsize)
+
+        var x = xmin
+        var low_resolution_count = 0
+        var (xl,yl) = xyxy.dequeue()
+    	var idx = 0
+        while(x <= xmax){
+            val (xr,yr) = xyxy.head
+            if(x < xl)
+                throw new BadJDXException("out of range")
+            else if(x <= xr) {
+                yy(idx) = if(x==xl) yl else yl+(yr-yl)/(xr-xl)*(x-xl)
+                x += xstep
+    			idx += 1
+                low_resolution_count += 1
+            } else {
+                val xlyl = xyxy.dequeue()
+                xl = xlyl._1
+                yl = xlyl._2
+                if(low_resolution_count > 0)
+                    low_resolution_count -= 1
+    		}
+    	}
+
+        if(low_resolution_count > 50)
+            throw new BadJDXException("resolution too low")
+        yy
+    }
+
+    def jdx2vec(filename:String):(Array[Float],String,String) = {
+        // read records and xyy data
+        val source = Source.fromFile(filename)
+        val f1 = source.getLines
+        val record = mutable.Map[String,String]()
+        val xyy = ArrayBuffer[(Float,Array[Float])]()
+        var inxyy = false
+        record("STATE") = ""
+        for( j <- f1 ) {
+            if( j.startsWith("##") ) {
+                val jsplit = j.split("=",2)
+                val varname  = jsplit(0).substring(2).trim()
+                val varvalue = jsplit(1).trim()
+                (varname,varvalue) match {
+                    case ("XYDATA", "(X++(Y..Y))") => inxyy = true
+                    case ("END",_) => inxyy = false
+                    case _ => record(varname) = varvalue
+                }
+            } else if(inxyy) {
+                // two numbers are not always splited by spaces, e.g.
+                // In some files there are strings like "12345-67890",
+                // which is 12345 and -67890
+                val jfix = j.replaceAll("-"," -")
+                val numbers = jfix.split(raw"\s+").map(_.toFloat)
+                val x = numbers(0)
+                val y = numbers.splitAt(1)._2
+                xyy += ( (x,y) )
+            }
+        }
+        source.close()
+        // further parse state
+        val (state1,state2) = parse_state(record("STATE"))
+        // generate [(x,y)..] data, x in 1/CM, y in transmittance
+        val xyxy = PriorityQueue.empty[(Float, Float)](implicitly[Ordering[(Float, Float)]].reverse)
+        val firstx = record("FIRSTX").toFloat
+        val lastx = record("LASTX").toFloat
+        val npoints = record("NPOINTS").toFloat
+        val deltax = (lastx-firstx)/(npoints-1)
+        val yfactor = record("YFACTOR").toFloat
+        val xunit = record("XUNITS")
+        val yunit = record("YUNITS")
+        for( (x,yy) <- xyy ) {
+            for( j <- Range(0,yy.length) ){
+                var _x = x + deltax*j
+                if( xunit == "MICROMETERS" )
+                    _x = 10000/_x
+                var _y = yy(j)*yfactor
+                if( yunit == "ABSORBANCE" )
+                    _y = pow(10,-_y).toFloat
+                xyxy.enqueue((_x,_y))
+            }
+        }
+        (standard(xyxy),state1,state2)
+    }
+
+    case class ExpIRAndState(mid:String, index:Int, vec:Array[Float], state:String, state_info:String)
+    case class MIDStruct(mid:String,structure:String)
+
+    def readExpIRAndState(filename:String):ExpIRAndState = {
+        // get mid and index
+        val ii = filename.split(raw"\.")(0).split("-")
+        val mid = ii(0)
+        val index = ii(1).toInt
+        try {
+            val (vec,state,state_info) = jdx2vec(inputdir+filename)
+            new ExpIRAndState(mid=mid,index=index,vec=vec,state=state,state_info=state_info)
+        } catch {
+            case _ : NoSuchElementException  => new ExpIRAndState(mid="",index=0,vec=Array[Float](),state="",state_info="")
+            case _ : BadJDXException => new ExpIRAndState(mid="",index=0,vec=Array[Float](),state="",state_info="")
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+        val session = SparkSession.builder.appName("04_create_expir_table").getOrCreate()
+        import session.implicits._
+
+        // process jdx files
+        val filelist = session.createDataset( ("ls "+inputdir !!).split(raw"\n") )
+        val expir_raw = filelist.map(readExpIRAndState)
+
+        // remove data of bad structures(structures not in mid_structure)
+        val mid_structure = session.read.parquet("outputs/03/mid_structure").as[MIDStruct]
+        val join = expir_raw.joinWith(mid_structure,expir_raw("mid")===mid_structure("mid"))
+        val expir = join.map(_._1)
+
+        // output
+        expir.show()
+        expir.write.parquet("outputs/04/expir")
+        val show_states = expir.groupBy(expir("state")).count().sort($"count".desc)
+        show_states.show()
+        println("done")
+    }
+
 }
-
-// convert x,y data to points at standard x
-//TODO: How to let PriorityQueue order ascending?
-def standard(xyxy:PriorityQueue[(Float,Float)]):Array[Float] = {
-    val xmin = 670 //included
-    val xmax = 3702 //included
-    val xstep = 4
-    val yy = Array[Float]( (xmax-xmin)/xstep + 1 )
-
-    var x = xmin
-    var low_resolution_count = 0
-    var (xl,yl) = xyxy.dequeue()
-	var idx = 0
-    while(x <= xmax){
-        val (xr,yr) = xyxy.head
-        if(x < xl)
-            return None //TODO: how to replace the return with scala correspondings?
-        else if(x <= xr) {
-            yy(idx) = if(x==xl) yl else yl+(yr-yl)/(xr-xl)*(x-xl)
-            x += xstep
-			idx += 1
-            low_resolution_count += 1
-        } else {
-            xl,yl = xyxy.dequeue()
-            if(low_resolution_count > 0)
-                low_resolution_count -= 1
-		}
-	}
-
-    if(low_resolution_count > 50)
-        return None //TODO: how to replace the return with scala correspondings?
-    yy
-}
-
-def jdx2vec(filename):
-    # read records and xyy data
-    f1 = open(filename)
-    record = {}
-    xyy = []
-    inxyy = False
-    record['STATE'] = 'unknown'
-    for j in f1:
-        if j[0:2] == '##':
-            varname = j.split('=')[0][2:].strip()
-            varvalue = j.split('=')[1].strip()
-            if varname == 'XYDATA' and varvalue == '(X++(Y..Y))':
-                inxyy = True
-                continue
-            if varname == 'END':
-                inxyy = False
-                continue
-            record[varname] = varvalue
-        elif inxyy:
-            j = j.replace('-',' -',9999)
-            numbers = j.split()
-            x = float(numbers[0])
-            y = [ float(yy) for yy in numbers[1:] ]
-            xyy.append((x,y))
-    f1.close()
-    # further parse state
-    state1,state2 = parse_state(record['STATE'])
-    # generate [(x,y)..] data, x in 1/CM, y in transmittance
-    xyxy = []
-    firstx = float(record['FIRSTX'])
-    lastx = float(record['LASTX'])
-    npoints = int(record['NPOINTS'])
-    deltax = (lastx-firstx)/(npoints-1)
-    yfactor = float(record['YFACTOR'])
-    xunit = record['XUNITS']
-    yunit = record['YUNITS']
-    for x,yy in xyy:
-        for j in range(len(yy)):
-            _x = x + deltax*j
-            if xunit == 'MICROMETERS':
-                _x = 10000/_x
-            _y = yy[j]*yfactor
-            if yunit == 'ABSORBANCE':
-                _y = 10**(-_y)
-            xyxy.append((_x,_y))
-    return standard(xyxy),state1,state2
-
-
-# now start
-from os import listdir
-from pyspark import SparkContext
-from pyspark.sql import SQLContext, Row
-
-sc = SparkContext(appName="03_create_expir_table")
-
-# read junk list
-fnjunk = '/home/gaoxiang/create-dataset-for-ir/outputs/02/all.junk.smi'
-junklist = [ i.split()[1] for i in open(fnjunk) ]
-
-# process jdx files
-inputdir = '/home/gaoxiang/create-dataset-for-ir/outputs/01/digitalized/'
-
-def process(i):
-    # get mid and index
-    ii = i.split('.')[0].split('-')
-    mid = ii[0]
-    index = int(ii[1])
-    # skip junk
-    if mid in junklist:
-        return None
-    vec,state,state_info = jdx2vec(inputdir+i)
-    # skip non-standardizable data
-    if vec is None:
-        return None
-    row = Row(mid=mid,index=index,vec=vec,state=state,state_info=state_info)
-    return row
-
-filelist = sc.parallelize(listdir(inputdir),200)
-rows = filelist.map(process).filter(lambda i: i is not None)
-
-# generate table
-sqlContext = SQLContext(sc)
-expir = sqlContext.createDataFrame(rows)
-expir.show()
-expir.write.parquet('outputs/03/expir')
-print('done')
