12da41bf86849149697ffc518d282ab2bfebb03b
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-08-05 10:00:09 -0400

    move to common.scala

--- tables.scala
+++
@@ -1,6 +0,0 @@
-package irms {
-    case class MIDStruct(mid:String,smiles:String)
-    case class ExpIRAndState(mid:String, index:Int, vec:Array[Float], state:String, state_info:String)
-    case class StructureUniverse(smiles:String,mass:Float)
-    case class TheoreticalIR(smiles:String, method:String, freqs:Array[(Float,Float)])
-}
