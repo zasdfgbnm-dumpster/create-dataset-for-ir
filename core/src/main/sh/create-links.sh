d631fec588ac896abaabc7b45fb29bd45cf1af6f
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-09-27 17:32:22 -0400

    moved to root

--- core/src/main/sh/create-links.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-set -e
-
-for i in $PWD/core/src/main/python/{calc-mass-fg,verify}.py;do
-	ln -sf $i ~/MEGA/bin
-done
-ln -sf $PWD/core/src/main/resources/FunctionalGroups.txt ~/MEGA/data
