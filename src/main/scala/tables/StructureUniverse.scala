e042f5459a163f124dff0fe0b390b666ffb9521c
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-10-12 12:45:54 -0400

    add source of structure to structure universe

--- src/main/scala/tables/StructureUniverse.scala
+++ src/main/scala/tables/StructureUniverse.scala
@@ -11,35 +11,49 @@ package irms {
         @fgexpand
         def apply(a:Array[Boolean]):FunctionalGroups = FunctionalGroups
     }
-    case class StructureUniverse(smiles:String,mass:Double, fg:FunctionalGroups)
+    case class StructureUniverse(smiles:String,mass:Double, fg:FunctionalGroups, source:Array[String])
     object StructureUniverse extends ProductTable[StructureUniverse] {
 
         private val universe_dir = Env.raw + "/universe"
 
-        private def parse(str:String):StructureUniverse = {
-            val (l,fgs) = str.split(raw"\s+",2+FunGrps.func_grps.length).splitAt(2)
+        private def parse(pyoutput:String,source:String):StructureUniverse = {
+            val (l,fgs) = pyoutput.split(raw"\s+",2+FunGrps.func_grps.length).splitAt(2)
             val fgsboolean = fgs.map(_.toBoolean)
-            StructureUniverse(smiles=l(0),mass=l(1).toDouble,fg=FunctionalGroupsCvt(fgsboolean))
+            StructureUniverse(smiles=l(0),mass=l(1).toDouble,fg=FunctionalGroupsCvt(fgsboolean),source=Array(source))
         }
 
         def create(path:String):Unit = {
             import Env.spark.implicits._
 
-            // get list of smiles
+            def smiles_to_table(smiles:Dataset[String],source:String):Dataset[StructureUniverse] = {
+                // apply transformations to smiles to generate parquet
+                val smstr = smiles.rdd.pipe(Env.pycmd + " " + Env.bin+"/calc-mass-fg.py").toDS()
+                smstr.map(parse(_,source))
+            }
+            // create table for smiles from NIST
             val mid_structure = TableManager.getOrCreate(MIDStruct)
-            val smiles_nist = mid_structure.map(_.smiles).distinct()
-            val filenames = ("ls "+universe_dir !!).trim
-            val smiles = if(filenames.length>0) {
-                val fn = filenames.split(raw"\n").map(universe_dir+"/"+_)
-                val smiles_universe = fn.map(Env.spark.read.text(_).as[String]).reduce(_.union(_))
-                smiles_nist.union(smiles_universe).repartition(2000).distinct()
-            } else smiles_nist
+            val nist = smiles_to_table(mid_structure.map(_.smiles).distinct(),"nist")
 
-            // apply transformations to smiles to generate parquet
-            val smstr = smiles.rdd.pipe(Env.pycmd + " " + Env.bin+"/calc-mass-fg.py").toDS()
-            val universe = smstr.map(parse)
+            // create table for smiles from external source
+            def fn_to_table(fn:String):Dataset[StructureUniverse] = {
+                val complete_path = universe_dir + "/" + fn
+                val smiles = Env.spark.read.text(complete_path).as[String].distinct()
+                smiles_to_table(smiles,fn)
+            }
+            val filenames = ("ls "+universe_dir !!).trim
+            val universe_dup = if(filenames.length>0) {
+                val external = filenames.split(raw"\n").map(fn_to_table).reduce(_.union(_))
+                nist.union(external)
+            } else nist
+
+            // merge array of sources by smiles
+            def merge_table_elem(a:StructureUniverse,b:StructureUniverse):StructureUniverse = {
+                StructureUniverse(smiles=a.smiles,mass=a.mass,fg=a.fg,source=(a.source++b.source).sorted)
+            }
+            val universe = universe_dup.map(j=>(j.smiles,j)).rdd.reduceByKey(merge_table_elem).map(_._2).toDS
 
             universe.show()
+            universe.groupBy("source").count().show()
             universe.write.parquet(path)
         }
 
