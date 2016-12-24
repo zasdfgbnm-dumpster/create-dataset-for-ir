5e5c2db3b54a7955e7c5fd5569cce0efcfe56210
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-12-24 01:43:16 -0500

    make same mid in the same usage

--- src/main/scala/tables/DatasetUsage.scala
+++ src/main/scala/tables/DatasetUsage.scala
@@ -17,6 +17,8 @@ package irms {
             val universe = TableManager.getOrCreate(StructureUniverse).select("smiles","source")//.as[StructureUniverse]
             val join = expir.join(midstruct,"mid").join(universe,"smiles").select("mid","index","state","source").as[Joined]
 
+            // now we only use gas phase data, therefore data of other state are set to "unused"
+            // because GDB13 is far from complete, all we can do is to use all data that happens to be inside GDB13 as level overall
             def assign_level(j:Joined):DatasetUsage = {
                 if(j.state=="gas") {
                     if(j.source.length>1)
@@ -34,10 +36,17 @@ package irms {
                 val train = split(0).map(j=>DatasetUsage(j.mid,j.index,j.level,"training"))
                 val valid = split(1).map(j=>DatasetUsage(j.mid,j.index,j.level,"validation"))
                 val test  = split(2).map(j=>DatasetUsage(j.mid,j.index,j.level,"testing"))
-                train.union(valid).union(test)
+                val all = train.union(valid).union(test)
+                // data with the same mid should have the same usage
+                val r = scala.util.Random
+                def rs(a:String,b:String):String = if(r.nextBoolean) a else b
+                val dups = all.map(j=>(j.mid,(j.usage,true))).rdd.reduceByKey( (i,j) => ( rs(i._1,j._1), (i._1==j._1) && i._2 && j._2 ) ).filter(!_._2._2).map(j=>(j._1,j._2._1)).toDS
+                val m = dups.collect().toMap
+                def fix_usage(du:DatasetUsage) = DatasetUsage(du.mid,du.index,du.level,if(m.contains(du.mid)) m(du.mid) else du.usage)
+                all.map(fix_usage)
             }
-            val overall = assign_usage(wlevels.filter(_.level=="overall"),Array(6.0,3.0,3.0))
-            val limited = assign_usage(wlevels.filter(_.level=="limited"),Array(5.0,1.0,1.0))
+            val overall = assign_usage(wlevels.filter(_.level=="overall"),Array(5.0,3.0,3.0))
+            val limited = assign_usage(wlevels.filter(_.level=="limited"),Array(4.0,1.0,1.0))
             val unused = wlevels.filter(_.level=="unused")
             val result = overall.union(limited).union(unused)
 
