ca1ef6ba6f334122445a2953c54757a78562d06f
Xiang Gao <qasdfgtyuiop@gmail.com>
2017-03-19 14:39:12 -0400

    add to mongodb insert

--- src/main/scala/CreateTables.scala
+++ src/main/scala/CreateTables.scala
@@ -6,7 +6,7 @@ import scala.concurrent.duration.Duration
 package irms {
 	object CreateTables {
 
-		def wait_results[C](observable:Observable[C]): Seq[C] = Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+		def wait_results[C](observable:Observable[C]): Seq[C] = Await.result(observable.toFuture(), Duration.Inf)
 
 		def create_tables(args: Array[String]): Unit = {
 			val arglist = (if(args.isEmpty) Array("DatasetUsage","MIDStruct","TheoreticalIR","ExpIRAndState","StructureUniverse") else args).toList
@@ -25,21 +25,24 @@ package irms {
 			}
 		}
 
+		case class SmExpir(smiles:String,vec:Array[Double],state:String,state_info:String)
+
 		def main(args: Array[String]): Unit = {
 
 			// show all tables
 			val spark = Env.spark
 			import spark.implicits._
-			val du = TableManager.getOrCreate(DatasetUsage).as[DatasetUsage]
-			val midstruct =  TableManager.getOrCreate(MIDStruct).as[MIDStruct]
-			val thir = TableManager.getOrCreate(TheoreticalIR).as[TheoreticalIR]
-			val expir =  TableManager.getOrCreate(ExpIRAndState).as[ExpIRAndState]
-			val univ = TableManager.getOrCreate(StructureUniverse).as[StructureUniverse]
-			du.show()
-			midstruct.show()
-			thir.show()
-			expir.show()
-			univ.show()
+
+			val du = TableManager.getOrCreate(DatasetUsage)//.as[DatasetUsage]
+			val midstruct =  TableManager.getOrCreate(MIDStruct)//.as[MIDStruct]
+			val thir = TableManager.getOrCreate(TheoreticalIR)//.as[TheoreticalIR]
+			val expir =  TableManager.getOrCreate(ExpIRAndState)//.as[ExpIRAndState]
+			val univ = TableManager.getOrCreate(StructureUniverse)//.as[StructureUniverse]
+			// du.show()
+			// midstruct.show()
+			// thir.show()
+			// expir.show()
+			// univ.show()
 
 			// convert to document of this form:
 			// 	{
@@ -68,14 +71,47 @@ package irms {
 			// 		]
 			// 	}
 
+			// convert expir to an rdd of Document arrays as shown above
+			val smexpir = expir.join(midstruct, expir("mid")===midstruct("mid")).select("smiles","vec","state","state_info").distinct().as[SmExpir]
+			def expir2doc(r:SmExpir):(String,Document) = (r.smiles,Document("expir"->List(Document("vec"->r.vec.toList,"state"->r.state,"state_info"->r.state_info))))
+			val smexpirkeydoc = smexpir.rdd.map(expir2doc).reduceByKey(_++_)
+
+			// convert thir to an rdd of Document arrays as shown above
+			def thir2doc(r:TheoreticalIR):(String,Document) = {
+				val freqs = r.freqs.map(j=>Document("freq"->j._1,"intensity"->j._2)).toList
+				(r.smiles,Document("thir"->List(Document("method"->r.method,"freqs"->freqs))))
+			}
+			val thirkeydoc = thir.as[TheoreticalIR].rdd.map(thir2doc).reduceByKey(_++_)
+
+			// convert mid struct map to an rdd of Document arrays as shown above
+			def midlist2doc(a:List[String]):Document = Document("external_id"->Document("nist"->a))
+			val midstructkeydoc = midstruct.as[MIDStruct].rdd.map(r=>(r.smiles,List(r.mid))).reduceByKey(_++_).mapValues(midlist2doc)
+
+			// convert universe to an rdd of Document arrays as shown above
+			def universe2doc(r:StructureUniverse):(String,Document) = {
+				def getval(f:java.lang.reflect.Field) = {
+					f.setAccessible(true)
+					f.get(r.fg)
+				}
+				val fg = r.fg.getClass.getDeclaredFields.map(j=>Document(j.getName->getval(j).asInstanceOf[Boolean])).reduce(_++_)
+				(r.smiles,Document("mass"->r.mass,"source"->r.source.toList,"fg"->fg))
+			}
+			val univkeydoc = univ.as[StructureUniverse].rdd.map(universe2doc _)
+
+			// merge thir, expir, external_id
+			val merged = smexpirkeydoc.union(thirkeydoc).union(midstructkeydoc).reduceByKey(_++_).union(univkeydoc).reduceByKey(_++_)
 
 			// export to MongoDB
-			val mongoClient: MongoClient = MongoClient()
-			val database: MongoDatabase = mongoClient.getDatabase("test")
-			val collection: MongoCollection[Document] = database.getCollection("test")
-			val doc: Document = Document("name" -> "MongoDB", "type" -> "database","count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
-			wait_results(collection.insertOne(doc))
-			println("done")
+			def add_to_db(docs:Iterator[Document]) = {
+				val docseq = docs.toSeq
+				val mongoClient: MongoClient = MongoClient()
+				val database: MongoDatabase = mongoClient.getDatabase("irms")
+				val collection: MongoCollection[Document] = database.getCollection("universe")
+				println("begin inserting " + docseq.length + "data into db, wait patiently...")
+				wait_results(collection.insertMany(docseq))
+				println("done inserting " + docseq.length + "data into db.")
+			}
+			merged.map(j=>Document("_id"->j._1)++j._2).foreachPartition(add_to_db)
 		}
 
 	}
